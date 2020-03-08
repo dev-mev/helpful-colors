a46bbe01cf8c6d0782c4157d750d498085a100ba
dev-mev <40147757+dev-mev@users.noreply.github.com>
2020-03-08 11:48:07 -0700

    remove yarn.lock

--- src/components/SwatchDetail.tsx
+++ src/components/SwatchDetail.tsx
@@ -3,24 +3,29 @@ import LeftSidebar from "./LeftSidebar";
 import { useParams } from "react-router-dom";
 
 function SwatchDetail(props: any) {
-  let { hex } = useParams();
+  const { hex } = useParams();
 
   return (
     <div className="app">
       <header className="header"></header>
       <LeftSidebar />
       <div className="swatch swatch-detail">
-        <div className="color-block" style={{ backgroundColor: "#" + hex }}></div>
+        <div
+          className="color-block"
+          style={{ backgroundColor: "#" + hex }}
+        ></div>
         <div className="color-label">{"#" + hex}</div>
       </div>
-      <button 
+      <button
         className="button back-button"
-        onClick={() => { props.history.push("/") }}
+        onClick={() => {
+          props.history.push("/");
+        }}
       >
         Back to all swatches
       </button>
     </div>
-  )
+  );
 }
 
 export default SwatchDetail;
